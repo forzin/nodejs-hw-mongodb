7f5cd131cda7dfa8a5434047429727127bfb3cf5
Danylo Korobko <dakorobko@gmail.com>
2024-12-27 11:32:15 +0100

    changed default 1 to 10

--- src/utils/parsePaginationParams.js
+++ src/utils/parsePaginationParams.js
@@ -10,7 +10,7 @@ const parseNumber = (number, defaultValue) => {
 
 export const parsePaginationParams = ({ page, perPage }) => {
     const parsedPage = parseNumber(page, 1);
-    const parsedPerPage = parseNumber(perPage, 1);
+    const parsedPerPage = parseNumber(perPage, 10);
 
     return {
         page: parsedPage,
